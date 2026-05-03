a5f205d66ef49d06f74d32431316fbc5ca89a896
surajinlab <suraj.in.lab@gmail.com>
2026-05-03 15:02:19 +0530

    employees data and csrf token

--- src/main/java/com/coder/securitydemo/controller/employee.java
+++ src/main/java/com/coder/securitydemo/controller/employee.java
@@ -1,16 +1,39 @@
 package com.coder.securitydemo.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping
 public class employee {
 
+    private List<Emp> emps = new ArrayList<>(List.of(
+            new Emp(1, "Gojo", 30000),
+            new Emp(2, "Geto", 24000)
+    ));
+
     @GetMapping("/home")
     public String greeting(HttpServletRequest request) {
         return "Welcome to My Project : " + request.getSession().getId() ;
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @GetMapping("/emps")
+    public List<Emp> getEmps() {
+        return emps;
+    }
+
+    @PostMapping("/emp")
+    public Emp addEmp(@RequestBody Emp emp) {
+        emps.add(emp);
+        return emp;
     }
 }
