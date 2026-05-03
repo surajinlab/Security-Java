d103a22965c99a8ce876a77002750f706abb2342
surajinlab <suraj.in.lab@gmail.com>
2026-05-03 20:56:48 +0530

    customizers and filterchain

--- src/main/java/com/coder/securitydemo/controller/config/SecurityConfig.java
+++ src/main/java/com/coder/securitydemo/controller/config/SecurityConfig.java
@@ -1,4 +1,26 @@
 package com.coder.securitydemo.controller.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public DefaultSecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
+
+        return http.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+        //http.formLogin(Customizer.withDefaults());
+    }
 }
