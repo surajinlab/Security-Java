0e0b338628fd7773455866c84418e23fd19f2dd8
surajinlab <suraj.in.lab@gmail.com>
2026-05-04 22:56:17 +0530

    userdetails with password

--- src/main/java/com/coder/securitydemo/controller/config/SecurityConfig.java
+++ src/main/java/com/coder/securitydemo/controller/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @Configuration
@@ -22,5 +26,24 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
         //http.formLogin(Customizer.withDefaults());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+         UserDetails user1 = User
+                 .withDefaultPasswordEncoder()
+                 .username("Itachi")
+                 .password("i@123")
+                 .roles("USER")
+                 .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("Madara")
+                .password("m@123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
